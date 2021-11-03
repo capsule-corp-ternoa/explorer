f623bc15e4b241b234b8d89d27c46d1aa380cdbe
Felliix <cdev9382@gmail.com>
2021-11-03 11:14:30 -0400

    update

--- pages/home-scan/HomeScan.tsx
+++ pages/home-scan/HomeScan.tsx
@@ -42,79 +42,77 @@ const DetailButton: React.FC<DetailButtonProps> = ({
 )
 
 const HomeScan: React.FC<HomeScanProps> = () => {
-    
-    const [summary, setSummary] = useState<any>(null)
+  const [summary, setSummary] = useState<any>(null)
+  const [latestBlocks, setLatestBlocks] = useState<any>(null)
+  const [nftTransfers, setNftTransfers] = useState<any>(null)
+  const [transfers, setTransfers] = useState<any>(null)
 
-    const [latestBlocks, setLatestBlocks] = useState<any>(null)
-    const [nftTransfers, setNftTransfers] = useState<any>(null)
-    const [transfers, setTransfers] = useState<any>(null)
+  useEffect(() => {
+    getSummary().then(setSummary).catch(() => {})
+    getBlockList(0, TABLE_ROWS).then(setLatestBlocks)
+    getNftTransferList(0, TABLE_ROWS).then(setNftTransfers)
+    getTransferList(0, TABLE_ROWS).then(setTransfers)
+  }, [])
 
-    useEffect(() => {
-      getSummary().then(setSummary).catch(() => {})
-      getBlockList(0, TABLE_ROWS).then(setLatestBlocks)
-      getNftTransferList(0, TABLE_ROWS).then(setNftTransfers)
-      getTransferList(0, TABLE_ROWS).then(setTransfers)
-    }, [])
+  return (
+    <Layout searchBar={false}>
+      <Summary
+        capsPrice={summary && summary.usd}
+        marketCap={summary && summary.usd_market_cap}
+        change24h={summary && summary.usd_24h_change}
+        transactions={null}
+        finalizedBlock={null}
+      />
 
-    return (
-      <Layout searchBar={false}>
-        <Summary
-          capsPrice={summary && summary.usd}
-          marketCap={summary && summary.usd_market_cap}
-          change24h={summary && summary.usd_24h_change}
-          transactions={null}
-          finalizedBlock={null}
-        />
-
-        <div className="row">
-          <div className="col-12">
-            <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
-              Latest Blocks
-            </div>
-            <ListView
-              columns={blockColumns}
-              renderCell={renderBlock}
-              data={latestBlocks && latestBlocks.data}
-              footer={(
-                <DetailButton href='/block' label='Show all Blocks' />
-              )}
-            />
+      <div className="row">
+        <div className="col-12">
+          <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
+            Latest Blocks
           </div>
-          <div className="col-12">
-            <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
-              NFT Transactions
-            </div>
-            <ListView
-              columns={nftTxColumns}
-              renderCell={renderNftTx}
-              data={nftTransfers && nftTransfers.data}
-              footer={(
-                <DetailButton href='/nft' label='Show all NFT' />
-              )}
-            />
-          </div>
-          <div className="col-12">
-            <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
-              Transfers
-            </div>
-            <ListView
-              columns={transferColumns}
-              renderCell={renderTransfer}
-              data={transfers && transfers.data}
-              footer={(
-                <DetailButton href='/trans' label='Show all Transfers' />
-              )}
-            />
-          </div>
-          <div className="col-12 col-md-6">
-            <TransactionChart data={statData} className='mt-4' />
-          </div>
-          <div className="col-12 col-md-6">
-            <BlockChart data={statData} className='mt-4' />
-          </div>
+          <ListView
+            columns={blockColumns}
+            renderCell={renderBlock}
+            data={latestBlocks && latestBlocks.data}
+            footer={(
+              <DetailButton href='/block' label='Show all Blocks' />
+            )}
+          />
         </div>
-      </Layout>
-    )
+        <div className="col-12">
+          <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
+            NFT Transactions
+          </div>
+          <ListView
+            columns={nftTxColumns}
+            renderCell={renderNftTx}
+            data={nftTransfers && nftTransfers.data}
+            footer={(
+              <DetailButton href='/nft' label='Show all NFT' />
+            )}
+          />
+        </div>
+        <div className="col-12">
+          <div className={clsx("title mt-4 mb-3", style.blockTitle)}>
+            Transfers
+          </div>
+          <ListView
+            columns={transferColumns}
+            renderCell={renderTransfer}
+            data={transfers && transfers.data}
+            footer={(
+              <DetailButton href='/trans' label='Show all Transfers' />
+            )}
+          />
+        </div>
+        <div className="col-12 col-md-6">
+          <TransactionChart data={statData} className='mt-4' />
+        </div>
+        <div className="col-12 col-md-6">
+          <BlockChart data={statData} className='mt-4' />
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default HomeScan;
