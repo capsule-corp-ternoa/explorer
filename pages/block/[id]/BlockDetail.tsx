e5b2748203c7d20779f050b6228c91fa7b89a325
dev-cool <cdev9382@gmail.com>
2021-11-15 12:55:05 +0900

    Switch back two tables in extrinsic & block details page

--- pages/block/[id]/BlockDetail.tsx
+++ pages/block/[id]/BlockDetail.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
 import Layout from 'components/base/Layout';
-import Down from 'components/assets/Down';
 import DetailView from 'components/base/DetailView';
 import ListView from 'components/base/ListView';
 import { eventColumns, eventRender, blockFields, extrinsicColumns, blockRender, extrinsicRender } from './table'
@@ -13,7 +12,6 @@ export interface AccountDetailProps {}
 const AccountDetail: React.FC<AccountDetailProps> = () => {
   const [data, setData] = useState<any>(null)
   const [data1, setData1] = useState<any>(null)
-  const [isEvent, setIsEvent] = useState<boolean>(false)
   const router = useRouter()
   const id = router.query.id as string
 
@@ -24,17 +22,6 @@ const AccountDetail: React.FC<AccountDetailProps> = () => {
     }
   }, [id])
 
-  useEffect(() => {
-    setIsEvent(false)
-  }, [])
-
-  const onClickParameters = () => {
-    setIsEvent(true)
-  }
-
-  const onClickEvents = () => {
-    setIsEvent(false)
-  }
   if (!id) {
     return null
   }
@@ -43,28 +30,14 @@ const AccountDetail: React.FC<AccountDetailProps> = () => {
     <Layout back='/block'>
       <h1 className="subTitle">Block #{id}</h1>
       <DetailView fields={blockFields} data={data} renderCell={blockRender}/>
-      { !isEvent &&
-        <>
-          <div className="d-flex mt-1">
-            <h1 className="subTitle1 mt-3">Extrinsics</h1>
-            <div className="down" onClick={() => onClickParameters()}>
-              <Down className="ms-3 mt-5 cursor-point"/>
-            </div>
-          </div>
-          <ListView columns={extrinsicColumns} data={data && data.extrinsic_detail} renderCell={extrinsicRender}/>
-        </>
-      }
-      { isEvent &&
-        <>
-          <div className="d-flex mt-1">
-            <h1 className="subTitle1 mt-3">Events({data1 && data1.totalCount})</h1>
-            <div className="down" onClick={() => onClickEvents()}>
-              <Down className="ms-3 mt-5 cursor-point"/>
-            </div>
-          </div>
-          <ListView columns={eventColumns} data={data1 && data1.data} renderCell={eventRender}/>
-        </>
-      }
+      <div className="d-flex mt-1">
+        <h1 className="subTitle1 mt-3">Extrinsics</h1>
+      </div>
+      <ListView columns={extrinsicColumns} data={data && data.extrinsic_detail} renderCell={extrinsicRender}/>
+      <div className="d-flex mt-1">
+        <h1 className="subTitle1 mt-3">Events({data1 && data1.totalCount})</h1>
+      </div>
+      <ListView columns={eventColumns} data={data1 && data1.data} renderCell={eventRender}/>
     </Layout>
   )
 }
