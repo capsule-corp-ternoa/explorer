d7bce5b7fe00c5b27bedfe1d252d021dc3fbeabf
dev-cool <cdev9382@gmail.com>
2021-11-18 02:08:33 +0900

    Update Footer

--- components/base/Footer/Footer.tsx
+++ components/base/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './Footer.module.scss';
+import FooterLogo from 'components/assets/FooterLogo';
 import Discord from 'components/assets/SocialMedias/Discord';
 import Telegram from 'components/assets/SocialMedias/Telegram';
 import Twitter from 'components/assets/SocialMedias/Twitter';
@@ -7,57 +8,9 @@ import Instagram from 'components/assets/SocialMedias/Instagram';
 
 const Footer: React.FC<{}> = () => {
     return(
-        <div className={style.footer + " row pt-3"}>
-            <div className={"col-md-6 col-12 mb-2 d-flex align-items-center justify-content-center justify-content-md-start"}>
-                <a
-                    href="https://discord.gg/cNZTGtGJNR"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    <Discord className={style.socialLogo}/>
-                </a>
-                <a
-                    href="https://t.me/ternoadiscussions"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    <Telegram className={style.socialLogo}/>
-                </a>
-                <a
-                    href="https://twitter.com/ternoa_"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    <Twitter className={style.socialLogo}/>
-                </a>
-                <a
-                    href="https://www.instagram.com/ternoa_/"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    <Instagram className={style.socialLogoRight}/>
-                </a>
-            </div>
-            <div className={"col-md-6 col-12 mb-2 d-flex align-items-center justify-content-center justify-content-md-end"}>
-                <a
-                    href="https://intercom.help/ternoa/fr/collections/2774679-legal"
-                    target="_blank"
-                    rel="noopener"
-                    className={style.footerLink}
-                >
-                    Terms
-                </a>
-                <a
-                    href="https://intercom.help/ternoa/fr/collections/2774679-legal"
-                    target="_blank"
-                    rel="noopener"
-                    className={style.footerLink}
-                >
-                    Privacy
-                </a>
-            </div>
-            <div className={"d-none d-md-block col-md-6 mb-2 d-flex flex-row align-items-center justify-content-left"}>
-                <p className={style.footerCopyright}>© 2021 Ternoa Scan developed and designed by ternoa.com. All rights reserved.</p>
+        <div className={style.footer + " row pt-3 mt-3"}>
+            <div className={"d-none d-md-flex col-md-12 mb-4 d-flex flex-row align-items-center justify-content-center"}>
+                <FooterLogo />
             </div>
             <div className={"d-md-none col-12 mb-2 d-flex flex-row align-items-center justify-content-center"}>
                 <p className={style.footerCopyright}>© 2021 Ternoa Scan. All rights reserved.</p>
