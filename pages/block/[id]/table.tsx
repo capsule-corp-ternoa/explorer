cbd16667090a6ec83376dbdc7301c831cd5a95ed
dev-cool <cdev9382@gmail.com>
2021-11-27 04:17:00 +0900

    Block detail mobile v2

--- pages/block/[id]/table.tsx
+++ pages/block/[id]/table.tsx
@@ -4,32 +4,32 @@ import CAPSDark from 'components/assets/CAPSDark';
 import Check from 'components/assets/Check';
 import Detail from 'components/assets/Detail';
 import Copy from 'components/assets/Copy';
-import { ellipsifyMiddle, formatSec } from 'helpers/lib';
+import { ellipsifyMiddle, ellipsifyLast, formatSec } from 'helpers/lib';
 
 export const blockFields = [
-  { text: 'Timestamp', dataKey: 'timestamp', className: 'text-left' },
+  { text: 'Timestamp', dataKey: 'timestamp', className: 'text-left', mobileClassName: 'col-12' },
   { text: 'Hash', dataKey: 'block_hash', className: 'text-left', mobileClassName: 'col-12' },
   { text: 'Parent Hash', dataKey: 'parent_hash', className: 'text-left', mobileClassName: 'col-12' },
   { text: 'State Root', dataKey: 'state_root', className: 'text-left', mobileClassName: 'col-12' },
   { text: 'Extrinsics Root', dataKey: 'extrinsics_root', className: 'text-left', mobileClassName: 'col-12' },
-  { text: 'Extrinsics', dataKey: 'extrinsics', className: 'text-left' },
-  { text: 'Total Module Events', dataKey: 'module_events', className: 'text-left' },
-  { text: 'Runtime Version', dataKey: 'runtime_version', className: 'text-left' },
-  { text: 'Block Time', dataKey: 'age', className: 'text-left' },
+  { text: 'Extrinsics', dataKey: 'extrinsics', className: 'text-left', mobileClassName: 'col-12' },
+  { text: 'Total Module Events', dataKey: 'module_events', className: 'text-left', mobileClassName: 'col-12' },
+  { text: 'Runtime Version', dataKey: 'runtime_version', className: 'text-left', mobileClassName: 'col-12' },
+  { text: 'Block Time', dataKey: 'age', className: 'text-left', mobileClassName: 'col-12' },
 ]
 
 export const extrinsicColumns = [
   { text: 'Extrinsic ID', dataKey: 'id', className: 'text-left' },
   { text: 'From', dataKey: 'from', className: 'text-left', mobileClassName: 'col-12' },
-  { text: 'Module', dataKey: 'module', className: 'text-left' },
-  { text: 'Call', dataKey: 'call', className: 'text-left' },
-  { text: 'Success', dataKey: 'success', className: 'text-left' },
+  { text: 'Module', dataKey: 'module', className: 'text-left only-desktop' },
+  { text: 'Call', dataKey: 'call', className: 'text-left only-desktop' },
+  { text: 'Success', dataKey: 'success', className: 'text-left only-desktop' },
   { text: '', dataKey: 'detail' },
 ]
 
 export const eventColumns = [
   { text: 'Event ID', dataKey: 'id', className: 'text-left' },
-  { text: 'Extrinsic Hash', dataKey: 'hash', className: 'text-left' },
+  { text: 'Extrinsic Hash', dataKey: 'hash', className: 'text-left only-desktop' },
   { text: 'Time', dataKey: 'age', className: 'text-left' },
   { text: 'Action', dataKey: 'action', className: 'text-left' },
 ]
@@ -75,13 +75,20 @@ export const extrinsicRender = (record: any, dataKey: string) => {
 
     case 'from':
       return (
-        <div className="d-flex">
-          <CAPSDark className="webIcon me-2" />
-          <span className="textToken mt-1">{record[dataKey]}</span>
-          <div className="ms-2 mt-1" onClick={()=>navigator.clipboard.writeText(record[dataKey])}>
-            <Copy className="cursor-point" />
+        <>
+          <div className="only-desktop">
+            <div className="d-flex">
+              <CAPSDark className="webIcon me-2" />
+              <span className="textToken mt-1">{record[dataKey]}</span>
+              <div className="ms-2 mt-1" onClick={()=>navigator.clipboard.writeText(record[dataKey])}>
+                <Copy className="cursor-point" />
+              </div>
+            </div>
           </div>
-        </div>
+          <div className="only-mobile">
+            <span className="textToken mt-1">{ellipsifyLast(record[dataKey])}</span>
+          </div>
+        </>
       )
 
     case 'success':
