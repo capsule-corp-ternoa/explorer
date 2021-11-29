ee29f5e230eb2a8d59f1e7a86d64273347de85e4
dev-cool <cdev9382@gmail.com>
2021-11-29 20:55:36 +0900

    Update account mobile v2

--- pages/account/table.tsx
+++ pages/account/table.tsx
@@ -3,6 +3,7 @@ import CAPSDark from 'components/assets/CAPSDark';
 import Copy from 'components/assets/Copy';
 import Detail from 'components/assets/Detail';
 import { FormattedNumber } from 'react-intl';
+import { ellipsifyLast } from 'helpers/lib';
 
 export const columns = [
   { text: 'Address', dataKey: 'address', className: 'text-left', mobileClassName: 'col-12' },
@@ -21,15 +22,24 @@ export const render = (record: any, dataKey: string) => {
       )
     case 'address':
       return (
-        <div className="d-flex">
-          <CAPSDark className="webIcon me-2" />
-          <Link href={`/account/${record[dataKey]}`}>
-          <a className="textToken mt-1">{record[dataKey]}</a>
-          </Link>
-          <div className="ms-2 mt-1" onClick={()=>navigator.clipboard.writeText(record[dataKey])}>
-            <Copy className="cursor-point" />
+        <>
+          <div className="only-desktop">
+            <div className="d-flex">
+              <CAPSDark className="webIcon me-2" />
+              <Link href={`/account/${record[dataKey]}`}>
+              <a className="textToken mt-1">{record[dataKey]}</a>
+              </Link>
+              <div className="ms-2 mt-1" onClick={()=>navigator.clipboard.writeText(record[dataKey])}>
+                <Copy className="cursor-point" />
+              </div>
+            </div>
           </div>
-        </div>
+          <div className="only-mobile">
+            <Link href={`/account/${record[dataKey]}`}>
+              <a className="textToken mt-1">{ellipsifyLast(record[dataKey])}</a>
+            </Link>
+          </div>
+        </>
       )
     case 'details':
     default:
