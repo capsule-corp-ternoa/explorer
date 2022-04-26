c880257da3cbd9f8b9b303f7b0a1a7f17ddd763f
Victor SALOMON <victor-salomon@hotmail.fr>
2022-04-26 12:31:52 +0200

    update header network

--- components/assets/Network/Network.tsx
+++ components/assets/Network/Network.tsx
@@ -2,10 +2,12 @@ import Link from "next/link"
 import style from './Network.module.scss';
 
 const Network : React.FC = ({}) => {
+    const toUpperCase = (word:any) => {return word.charAt(0).toUpperCase() + word.slice(1);}
+    const network = toUpperCase(process.env.NEXT_PUBLIC_ENV)
     return(
         <Link href='https://ternoa.com/'>
             <a className={style.wrapper} target="_blank" rel="noreferrer noopener" >
-                <span className={style.title}>Mainnet</span>
+                <span className={style.title}>{network ? network : "Mainnet"}</span>
                 <div className={style.connectIndicator}>
                     <div className={style.outterIndicator} />
                     <div className={style.innerIndicator} />
